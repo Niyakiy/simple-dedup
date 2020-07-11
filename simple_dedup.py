9c9b557fce484a9e64571418405fb0980f80aecf
Eugene Chepurniy <eugene.chepurniy@logianalytics.com>
2020-07-11 12:51:28 +0300

    Apply show-same-size-only option

--- simple_dedup.py
+++ simple_dedup.py
@@ -7,9 +7,10 @@ Usage:
   simple_dedup.py <DIR> [options]
 
 Options:
-  --help     Show help
-  --verbose  Add some debugging noise
-  --exclude-extentions=<>
+  --help                       Show help
+  --verbose                    Add some debugging noise
+  --exclude-extentions=<EXTs>  CSV extensions to remove from dudup
+  --show-same-size-only
 """
 
 import collections
@@ -20,20 +21,19 @@ from docopt import docopt
 LOGGER = logging.getLogger(__name__)
 
 
-def load_directory_content(path):
+def filter_by_extensions(path, excluded_extensions):
     """
-    Scan whole dir/subdirs and return files
+    Filter if path's file extension isn't in excluded_extensions
 
     :param path:
+    :param excluded_extensions:
     :return:
     """
-    return_files = []
-    for directory, subdirectories, files in walk(path):
-        LOGGER.warning('Processing dir %s', directory)
-        return_files.extend(
-            [(directory, file) for file in files]
-        )
-    return return_files
+    for ext in excluded_extensions:
+        if path.endswith(ext):
+            return False
+
+    return True
 
 
 class DedupDirectoryProcessor:
@@ -41,7 +41,7 @@ class DedupDirectoryProcessor:
     Class to locate duplicated files
     """
 
-    def __init__(self, abs_path):
+    def __init__(self, abs_path, excluded_extensions=None, same_size_only=False):
         """
         Init
 
@@ -50,6 +50,8 @@ class DedupDirectoryProcessor:
         self.file_list = []
         self.file_count = 0
         self.path = abs_path
+        self.excluded_extensions = list(excluded_extensions) if excluded_extensions else []
+        self.same_size_only = same_size_only
         self.duplicate_with_counts = {}
 
     def load_directory(self):
@@ -58,7 +60,17 @@ class DedupDirectoryProcessor:
 
         :return:
         """
-        self.file_list = load_directory_content(self.path)
+        for directory, subdirectories, files in walk(self.path):
+            LOGGER.warning('Processing dir %s', directory)
+            if self.excluded_extensions:
+                self.file_list.extend(
+                    [(directory, file) for file in files if filter_by_extensions(
+                        file, self.excluded_extensions)]
+                )
+            else:
+                self.file_list.extend(
+                    [(directory, file) for file in files]
+                )
         self.file_count = len(self.file_list)
         LOGGER.warning('Found %i files.' % self.file_count)
 
@@ -83,24 +95,53 @@ class DedupDirectoryProcessor:
         for count_info in self.duplicate_with_counts:
             print('-'*30)
             print("File:", count_info[0])
-            all_file_locations = [
-                f"{file_info[0]}/{file_info[1]}" for file_info in filter(
-                    lambda x: x[1] == count_info[0],
-                    self.file_list
-                )
-            ]
-            print('\n'.join([f"{file} (size:{path.getsize(file)})" for file in sorted(
-                all_file_locations,
-                key=path.getsize,
+            all_files_info = list(sorted(
+                map(
+                    lambda x: (x, path.getsize(x)),
+                    [f"{file_info[0]}/{file_info[1]}" for file_info in filter(
+                        lambda x: x[1] == count_info[0],
+                        self.file_list
+                    )]
+                ),
+                key=lambda x: x[1],
                 reverse=True
-            )]))
+            ))
+
+            if self.same_size_only:
+                same_sizes_counter = list(map(
+                    lambda x: x[0],
+                    filter(
+                        lambda x: x[1] > 1,
+                        collections.Counter([
+                            file_info[1] for file_info in all_files_info
+                        ]).items()
+                    )
+                ))
+
+                same_sizes_files = list(filter(
+                    lambda x: x[1] in same_sizes_counter,
+                    all_files_info
+                ))
+
+                if same_sizes_files:
+                    print('\n'.join([f"{file_info[0]} (size: {file_info[1]})" for file_info in same_sizes_files]))
+            else:
+                # Simply print all found files
+                print('\n'.join([f"{file_info[0]} (size: {file_info[1]})" for file_info in all_files_info]))
 
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
     if arguments['--verbose']:
         print("Script's arguments: ", arguments)
-    d = DedupDirectoryProcessor(arguments['<DIR>'])
+    exclusions = []
+    if arguments['--exclude-extentions']:
+        exclusions = arguments['--exclude-extentions'].split(',')
+    d = DedupDirectoryProcessor(
+        arguments['<DIR>'],
+        excluded_extensions=exclusions,
+        same_size_only=arguments['--show-same-size-only']
+    )
     d.load_directory()
     d.search_same_name()
     d.make_duplicates_list()
